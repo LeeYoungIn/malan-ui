051beca7655427bd1606baed60edd4c60eb562f3
이영인 <lyi@malan.kr>
2024-03-15 01:24:13 +0900

    component/badge css

--- app/ui/components/badge.tsx
+++ app/ui/components/badge.tsx
@@ -13,9 +13,10 @@ export function Badge({ size = 'base', variant, withIcon = false, className, ...
     className={clsx('badge font-light rounded-full',
       {
         'px-3 py-1 text-sm': !withIcon && size === 'base',
-        'px-2 py-0.5 text-xs': size === 'sm',
+        'px-2 py-0.5': withIcon || size === 'sm',
         'px-4 py-1 text-base': size === 'lg',
-        'flex items-center gap-2': withIcon,
+        'text-xs': size === 'sm',
+        'flex items-center gap-1 [&>i]:text-sm': withIcon,
         'bg-stone-200 dark:bg-white hover:bg-stone-300 dark:disabled:bg-stone-400': variant !== 'clean'
       },
       className)}/>
