7e981f39c594ab822e527a94ec0f6aba3dcd445e
이영인 <lyi@malan.kr>
2024-03-18 14:51:21 +0900

    modal: dark mode

--- app/ui/components/modal.tsx
+++ app/ui/components/modal.tsx
@@ -15,7 +15,7 @@ export function Modal({ visible = true, backdrop = true, title, onClose, childre
     className="fixed inset-0 flex items-center justify-center w-full h-screen bg-stone-200/50 dark:bg-stone-800/50 backdrop-blur">
     <div className="modal min-w-60">
       <div className="modal-header flex items-center justify-between gap-6">
-        <span>{title}</span>
+        <span className="text-xl">{title}</span>
         <Button variant="clean" className="modal-close" onlyIcon></Button>
       </div>
       <div className="modal-body">
