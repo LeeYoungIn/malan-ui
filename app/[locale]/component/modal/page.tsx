576ada804ce2515e04e6f4d86358707c1d1393b2
이영인 <lyi@malan.kr>
2024-03-20 17:19:27 +0900

    modal with footer

--- app/[locale]/component/modal/page.tsx
+++ app/[locale]/component/modal/page.tsx
@@ -4,16 +4,50 @@ import { Button, Modal } from '@ui/components'
 
 export default function () {
   const [modal1, toggleModal1] = useState(false)
+  const [modal2, toggleModal2] = useState(false)
+  const [modal3, toggleModal3] = useState(false)
+
   return <>
     <section>
       <h3>Default</h3>
       <div className="grid place-items-center">
-        <Button onClick={() => toggleModal1(true)}>Open</Button>
-        <Modal
-          title="Modal Title"
-          visible={modal1} onClose={() => toggleModal1(false)}>
-          <p>This is Modal 1.</p>
-        </Modal>
+        <div>
+          <Button onClick={() => toggleModal1(true)}>Open</Button>
+          <Modal
+            title="Modal Title"
+            visible={modal1} onClose={() => toggleModal1(false)}>
+            <p>This is Modal 1.</p>
+          </Modal>
+        </div>
+      </div>
+    </section>
+
+    <section>
+      <h3>With Footer</h3>
+      <div className="flex flex-wrap items-center justify-center gap-4">
+        <div>
+          <Button onClick={() => toggleModal2(true)}>OK</Button>
+          <Modal
+            title="Modal Title"
+            visible={modal2}
+            footer={<Button variant="clean">OK</Button>}
+            onClose={() => toggleModal2(false)}>
+            <p>This is Modal 1.</p>
+          </Modal>
+        </div>
+        <div>
+          <Button onClick={() => toggleModal3(true)}>OK & Cancel</Button>
+          <Modal
+            title="Modal Title"
+            visible={modal3}
+            footer={<>
+              <Button variant="clean">OK</Button>
+              <Button variant="clean">Cancel</Button>
+            </>}
+            onClose={() => toggleModal3(false)}>
+            <p>This is Modal 1.</p>
+          </Modal>
+        </div>
       </div>
     </section>
   </>
--- app/ui/components/modal.tsx
+++ app/ui/components/modal.tsx
@@ -3,13 +3,13 @@ import { Button } from '@ui/components/button'
 
 export interface ModalProps {
   visible?: boolean
-  backdrop?: boolean
   title?: string
   children: ReactNode
+  footer?: ReactNode
   onClose?(): void
 }
 
-export function Modal({ visible = true, backdrop = true, title, onClose, children }: ModalProps) {
+export function Modal({ visible = true, title, footer, onClose, children }: ModalProps) {
   return !visible ? null : <div
     onClick={onClose}
     className="fixed inset-0 flex items-center justify-center w-full h-screen bg-stone-200/50 dark:bg-stone-800/50 backdrop-blur">
@@ -21,9 +21,9 @@ export function Modal({ visible = true, backdrop = true, title, onClose, childre
       <div className="modal-body">
         {children}
       </div>
-      <div className="modal-footer grid auto-cols-fr divide-x dark:divide-stone-800">
-        <Button variant="clean">OK</Button>
-      </div>
+      {footer && <div className="modal-footer grid grid-flow-col auto-cols-fr divide-x dark:divide-stone-800">
+        {footer}
+      </div>}
     </div>
   </div>
 }
